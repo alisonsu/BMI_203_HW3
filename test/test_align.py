60bbf1a6975c80ba74cf91347f361fb95d558d65
Alison Su <alison.nc.su@gmail.com>
2017-02-23 14:27:44 -0800

    Added a few more test cases

--- test/test_align.py
+++ test/test_align.py
@@ -26,3 +26,24 @@ def test_traceback():
     assert(a1 == "ACV")
     assert(a2 == "ACV")
 
+def test_case_dependency():
+    
+    seq1 = "aCv"
+    seq2 = "AcV"
+
+    score_matrix, key = align.read_score_matrix("BLOSUM50")
+    max_score, a1, a2 = align.SWalign_return_static(seq1,seq2,10,1,score_matrix,key)
+
+    assert(a1 == "aCv")
+    assert(a2 == "AcV")
+
+def test_read_score_matrix():
+    
+    answer =  "ARNDCQEGHILKMFPSTWYVBZX*"
+    size = 576
+    
+    score_matrix, key = align.read_score_matrix("BLOSUM50")
+    keystring = "".join(key)
+
+    assert(keystring == answer)
+    assert (np.size(score_matrix) == size)
